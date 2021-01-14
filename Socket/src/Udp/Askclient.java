113474a0b3323965fb65a6140520a5f6e5a3695c
renyujie <renyj7@lenovo.com>
2021-01-14 23:12:09 +0800

    1.14 Socket

--- Socket/src/Udp/Askclient.java
+++ Socket/src/Udp/Askclient.java
@@ -2,20 +2,31 @@ package Udp;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class Askclient {
     public static void main(String[] args) throws IOException {
         //创建socket发送和接收数据包
         DatagramSocket socket = new DatagramSocket(9999);
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            //发送数据包
+            String str = sc.nextLine();//以回车作为标志
+            byte[] buf = str.getBytes();
+            InetAddress adress = InetAddress.getLocalHost();
+            int port = 8888;  //与服务器端口一致
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, adress,port );
+            socket.send(packet);
+            if ("bye".equals(str)){
+                break; //server说bye结束循环，socket.close();
+            }
 
-        //发送数据包
-        String str = "hello";
-        byte[] buf = str.getBytes();
-        InetAddress adress = InetAddress.getLocalHost();
-        int port = 8888;  //与服务器端口一致
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, adress,port );
-        socket.send(packet);
-
+            //接收服务器的反馈
+            byte[] buf2 = new byte[128];
+            DatagramPacket fankui = new DatagramPacket(buf2, buf.length);
+            socket.receive(fankui);
+            System.out.println(new String(fankui.getData(),0, fankui.getLength()));
+        }
         //关闭socket
         socket.close();
 
--- Socket/src/Udp/Askserver.java
+++ Socket/src/Udp/Askserver.java
@@ -3,26 +3,37 @@ package Udp;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.Scanner;
 
 public class Askserver {
     public static void main(String[] args) throws IOException {
         //创建socket，用来发送和接收数据包
         DatagramSocket socket = new DatagramSocket(8888);
+        Scanner sc = new Scanner(System.in);
 
-        //接收数据
-        byte[] buf = new byte[128];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
+        while (true){
+            //接收数据
+            byte[] buf = new byte[128];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            String result = new String(packet.getData(), 0, packet.getLength());
+            System.out.println(result);
+            if ("bye".equals(result)){
+                break;////客户说bye结束循环，socket.close();
+            }
+//            System.out.println(new String(packet.getData()));//这样会把128剩余的也打印成空白，这是不太好的
+//            System.out.println(new String(packet.getData(), 0, packet.getLength()));
+//            System.out.println(packet.getLength());
+//            System.out.println(packet.getPort());//9999 对方接收数据端口
+//            System.out.println(packet.getAddress());//对方的ip地址
 
-        //System.out.println(new String(packet.getData()));//这样会把128剩余的也打印成空白，这是不太好的
-        System.out.println(new String(packet.getData(), 0, packet.getLength()));
-        System.out.println(packet.getLength());
-        System.out.println(packet.getPort());//9999 对方接收数据端口
-        System.out.println(packet.getAddress());//对方的ip地址
-
+            //发送响应
+            String str = sc.nextLine();
+            byte[] buf2 = str.getBytes();
+            DatagramPacket fanhui = new DatagramPacket(buf2, buf2.length, packet.getAddress(), packet.getPort());
+            socket.send(fanhui);
+        }
         //关闭socket
         socket.close();
-
-
     }
 }
